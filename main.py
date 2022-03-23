354cf2b26ffa3a6272ff77adba4f39cb93d1005b
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-03-23 18:10:11 +0100

    added events. enemy killed, downed, arrested, in custody, level load

--- main.py
+++ main.py
@@ -14,23 +14,53 @@ def read_root():
 async def takes_frever():
     await asyncio.sleep(200)
 
+@app.get("/event/level_load")
+async def read_item():#, q: Optional[str] = None
+    t = asyncio.create_task(takes_frever())
+    print(f"{datetime.datetime.now()} level_load")
+    return {"received"}
+
 @app.get("/event/shield/{value}")
 async def read_item(value: int):#, q: Optional[str] = None
     t = asyncio.create_task(takes_frever())
     print(f"{datetime.datetime.now()} set shield with value {value}")
-    return {"success"}
+    return {"received"}
 
 @app.get("/event/health/{value}")
-async def read_item(value: int):#, q: Optional[str] = None
+async def read_item(value: int):
     t = asyncio.create_task(takes_frever())
     print(f"{datetime.datetime.now()} set health with value {value}")
-    return {"success"}
+    return {"received"}
 
 @app.get("/event/damage_taken")
-async def read_item():#, q: Optional[str] = None
+async def read_item():
     t = asyncio.create_task(takes_frever())
     print(f"{datetime.datetime.now()} damage_taken")
-    return {"success"}
+    return {"received"}
+
+@app.get("/event/downed")
+async def read_item():
+    t = asyncio.create_task(takes_frever())
+    print(f"{datetime.datetime.now()} downed")
+    return {"received"}
+
+@app.get("/event/arrested")
+async def read_item():
+    t = asyncio.create_task(takes_frever())
+    print(f"{datetime.datetime.now()} arrested")
+    return {"received"}
+
+@app.get("/event/custody")
+async def read_item():
+    t = asyncio.create_task(takes_frever())
+    print(f"{datetime.datetime.now()} custody")
+    return {"received"}
+
+@app.get("/evaluate/killshot")
+async def read_item():
+    t = asyncio.create_task(takes_frever())
+    print(f"{datetime.datetime.now()} enemy killshot")
+    return {"received"}
 
 if __name__ == "__main__":
     import uvicorn
