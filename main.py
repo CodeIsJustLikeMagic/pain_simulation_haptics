3bc96de0ee5a3459197397c4e89c099d91ca60b3
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-07-27 20:24:59 +0200

    setup saving evaluation stats in csv

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import asyncio
 from typing import Optional
 import datetime
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
+eval_df = pd.DataFrame(columns=['Player', 'Profile', 'Time', 'Event'])
+
+player = "test"
+profile = "profile1"
+
+level_start = datetime.datetime.now()
 
 @app.get("/")
 def read_root():
@@ -56,10 +63,53 @@ async def read_item():
     print(f"{datetime.datetime.now()} custody")
     return {"received"}
 
+
+
+def documentAction(action):
+    eval_df.loc[len(eval_df.index)] = [player, profile, datetime.datetime.now(), action]
+
+@app.get("/evaluate/levelload")
+async def read_item():
+    global eval_df
+    global level_start
+    eval_df = pd.DataFrame(columns=['Player', 'Profile', 'Time', 'Event'])
+    level_start = datetime.datetime.now()
+    documentAction("LevelLoad")
+    documentAction("killed an enemy")
+    return {"received"}
+
+@app.get("/evaluate/levelquit")
+async def read_item():
+
+    documentAction("LevelQuit")
+    eval_df.to_csv(player+"_"+profile+"_"+str(level_start.strftime("%m_%d_%y__%H_%M"))+".csv")
+
+    documentAction("killed an enemy")
+    return {"received"}
+
 @app.get("/evaluate/killshot")
 async def read_item():
-    t = asyncio.create_task(takes_frever())
-    print(f"{datetime.datetime.now()} enemy killshot")
+    documentAction("killed an enemy")
+    return {"received"}
+
+@app.get("/evaluate/unshielded")
+async def read_item():
+    documentAction("unshielded hit")
+    return {"received"}
+
+@app.get("/evaluate/shielded")
+async def read_item():
+    documentAction("shielded hit")
+    return {"received"}
+
+@app.get("/evaluate/downed")
+async def read_item():
+    documentAction("downed")
+    return {"received"}
+
+@app.get("/evaluate/revived")
+async def read_item():
+    documentAction("revived")
     return {"received"}
 
 if __name__ == "__main__":
